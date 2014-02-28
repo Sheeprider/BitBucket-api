be45515d506d87f14807a676f3c2f20d79674b75
Baptiste Millou <baptiste@smoothie-creative.com>
2014-02-28 14:51:34 -0500

    Version bump to 0.5.0
    Signed-off-by: Baptiste Millou <baptiste@smoothie-creative.com>

--- bitbucket/__init__.py
+++ bitbucket/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.4.5'
+__version__ = '0.5.0'
 
 __doc__ = """
 Bitbucket has a REST API publicly available, this package provide methods to interact with it.
